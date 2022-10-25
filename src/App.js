6da637f652fa10fb73990c1f941869b44998bfd7
Denis Poplavskii <normal64@mail.ru>
2022-10-25 19:02:02 +0200

    feat: basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import chroma from "chroma-js"
+import './App.scss';
+
 
 function App() {
+  useEffect(() => {
+    console.log(`useEffect`,chroma.random());
+
+    setRandomColors()
+    return () => {
+      
+    }
+  }, [])  
+  
+  function generateRandomColor(){
+    const hexCodes = "0123456789ABCDEF"
+    let color = ""
+    for(let i = 0 ; i < 6 ; i++){
+      color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return "#" + color
+  }
+  function setRandomColors(){
+    const cols = document.querySelectorAll('.col')
+    cols.forEach(col=>{
+      //let color = generateRandomColor()
+      let color = chroma.random()
+      col.style.background = color; 
+      let text = col.querySelector("h2")
+      let button = col.querySelector("button")
+      text.textContent = color;
+      setTextColor(text,color)
+      setTextColor(button,color)
+    })
+  }
+  function setTextColor(text,color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5  ? "black" : "white"
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="colors">
+      <div className="col" >
+        <h2>Text</h2>
+        
+        <button>
+          <i className="fa-solid fa-lock-open"></i>
+        </button>
+      </div>
+      <div className="col">
+        <h2>Text</h2>
+        <button>
+          <i className="fa-solid fa-lock-open"></i>
+        </button>
+      </div>
+      <div className="col">
+        <h2>Text</h2>
+        <button>
+          <i className="fa-solid fa-lock-open"></i>
+        </button>
+      </div>
+      <div className="col">
+        <h2>Text</h2>
+        <button>
+          <i className="fa-solid fa-lock-open"></i>
+        </button>
+      </div>
+      <div className="col">
+        <h2>Text</h2>
+        <button>
+          <i className="fa-solid fa-lock-open"></i>
+        </button>
+      </div>
     </div>
   );
 }
