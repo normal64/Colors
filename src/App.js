bbab714724f5fe055b1789cf0adb5114693d8a0d
Denis Poplavskii <normal64@mail.ru>
2022-10-29 18:16:11 +0200

    feat: lock function on click

--- src/App.js
+++ src/App.js
@@ -1,76 +1,90 @@
-import { useEffect } from 'react';
-import chroma from "chroma-js"
-import './App.scss';
-
+import { useState, useEffect } from "react";
+import chroma from "chroma-js";
+import "./App.scss";
 
 function App() {
+  const [rerender, setRerender] = useState(false);
   useEffect(() => {
-    console.log(`useEffect`,chroma.random());
-
-    setRandomColors()
-    return () => {
+    console.log(`useEffect`, chroma.random());
+    setRandomColors();
+    return () => {};
+  }, []);
+  function lockIt(e) {
+    console.log("icon click",e.target.className);
+    if(e.target.className.includes("fa-lock-open")){
       
-    }
-  }, [])  
-  
-  function generateRandomColor(){
-    const hexCodes = "0123456789ABCDEF"
-    let color = ""
-    for(let i = 0 ; i < 6 ; i++){
-      color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    return "#" + color
+      e.target.className = "icon click fa-solid fa-lock" 
+    }else e.target.className = "icon click fa-solid fa-lock-open" 
+    //e.target.className = "icon click fa-solid fa-lock" 
   }
-  function setRandomColors(){
-    const cols = document.querySelectorAll('.col')
-    cols.forEach(col=>{
-      //let color = generateRandomColor()
-      let color = chroma.random()
-      col.style.background = color; 
-      let text = col.querySelector("h2")
-      let button = col.querySelector("button")
-      text.textContent = color;
-      setTextColor(text,color)
-      setTextColor(button,color)
-    })
-  }
-  function setTextColor(text,color){
-    const luminance = chroma(color).luminance()
-    text.style.color = luminance > 0.5  ? "black" : "white"
+  document.addEventListener("keydown", (event) => {
+    console.log("effect", event);
+    if (event.code === "Space") {
+      console.log("spacebar pressed");
+      setRandomColors();
+    }
+  });
 
+  function generateRandomColor() {
+    const hexCodes = "0123456789ABCDEF";
+    let color = "";
+    for (let i = 0; i < 6; i++) {
+      color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+    }
+    return "#" + color;
   }
- 
+  function setRandomColors() {
+    const cols = document.querySelectorAll(".col");
+    cols.forEach((col) => {
+      //let color = generateRandomColor()
+      let color = chroma.random();
+      col.style.background = color;
+      let text = col.querySelector("h2");
+      let button = col.querySelector("button");
+      text.textContent = color;
+      setTextColor(text, color);
+      setTextColor(button, color);
+    });
+  }
+  function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? "black" : "white";
+  }
+
   return (
-    <div className="colors">
-      <div className="col" >
+    <div
+      className="colors"
+      //onKeyPress={(event) =>handleSpacePress(event)}
+    >
+      <div className="col">
         <h2>Text</h2>
-        
-        <button>
-          <i className="fa-solid fa-lock-open"></i>
+
+        <button data-type="lock">
+          <i className="fa-solid fa-lock-open" onClick={(e) => lockIt(e)}></i>
         </button>
       </div>
       <div className="col">
         <h2>Text</h2>
         <button>
-          <i className="fa-solid fa-lock-open"></i>
+          <i className="fa-solid fa-lock-open"  onClick={(e) => lockIt(e)}></i>
         </button>
       </div>
       <div className="col">
         <h2>Text</h2>
         <button>
-          <i className="fa-solid fa-lock-open"></i>
+          <i className="fa-solid fa-lock-open"  onClick={(e) => lockIt(e)}></i>
         </button>
       </div>
       <div className="col">
         <h2>Text</h2>
         <button>
-          <i className="fa-solid fa-lock-open"></i>
+          <i className="fa-solid fa-lock-open"  onClick={(e) => lockIt(e)}></i>
         </button>
       </div>
       <div className="col">
         <h2>Text</h2>
         <button>
-          <i className="fa-solid fa-lock-open"></i>
+          <i className="fa-solid fa-lock-open"  onClick={(e) => lockIt(e)}></i>
         </button>
       </div>
     </div>
